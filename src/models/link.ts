63b66998be74f8081a3301e00872f891f61027b9
1shailendra2 <bhandarishailesh81@gmail.com>
2025-10-21 23:58:10 +0545

    linkroute

--- src/models/link.ts
+++ src/models/link.ts
@@ -1,10 +1,12 @@
 import mongoose, {Schema, Document} from "mongoose";
 export interface ILink extends Document { 
  userId: mongoose.Types.ObjectId;
+ contentId: mongoose.Types.ObjectId;
  hash: string;
 }
 const LinkSchema= new Schema<ILink>({
     userId: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    contentId: {type: Schema.Types.ObjectId, ref: "Content", required: true},
     hash: {type: String, required: true, unique: true},
 },
 {timestamps: true}
--- src/routes/link.routes.ts
+++ src/routes/link.routes.ts
@@ -1,4 +1,25 @@
-import {authMiddleware} from "../middleware/m.middleware";
+import express , {Request, Response} from "express";
+import {authMiddleware , AuthenticatedRequest} from "../middleware/m.middleware";
 import {Router} from "express";
-const router= Router();
-r
+import {LinkModel} from "../models/link";
+const router= express.Router();
+
+router.post("/link/share", authMiddleware, async(req:AuthenticatedRequest, res: Response)=>{
+        const {share }= req.body;
+        const userId= req.userId;
+    try{
+        if(share){
+            const hash= Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+            const newLink= await LinkModel.create({userId, hash});
+            return res.status(201).json({message: "link shared", link: newLink})
+        }
+        else {
+            await LinkModel.findOneAndDelete({userId});
+            return res.status(200).json({message:" link not shared"});
+        }
+    }
+    catch(error){
+        console.error("error", error);
+        return res.status(500).json({error: "Internal server error"});
+    }
+});
